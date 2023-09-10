4b7012032a100520cc01bdcb2ad3feff36415708
Akeem57 <lyrlandais@hotmail.com>
2023-09-10 07:59:03 +0200

    debut

--- air04.rb
+++ air04.rb
@@ -4,8 +4,8 @@ UN SEUL A LA FOIS
 Créez un programme qui aﬃche une chaîne de caractères en évitant les
 caractères identiques adjacents.
 Exemples d’utilisation :
-$> python exo.py “Hello milady,
+$> python exo.py “Hello milady, bien ou quoi ??”
 Helo milady, bien ou quoi ?
-bien ou quoi ??”
+
 Aﬃcher error et quitter le programme en cas de problèmes d’arguments.
 =end
