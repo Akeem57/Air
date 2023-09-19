122b3dff53c9b6f94cbdcc555d06d1cea4c6f937
Akeem57 <lyrlandais@hotmail.com>
2023-09-19 19:26:11 +0200

    Un Seul à la Fois

--- air04.rb
+++ air04.rb
@@ -9,3 +9,47 @@ Helo milady, bien ou quoi ?
 
 Aﬃcher error et quitter le programme en cas de problèmes d’arguments.
 =end
+
+def clean_word(word)
+    cleaned_word = ""
+    j = 0
+  
+    while j < word.length
+      if word[j] != word[j + 1] || j == word.length - 1
+        cleaned_word += word[j]
+      end
+      j += 1
+    end
+  
+    return cleaned_word
+end
+  
+ 
+  def clean_sentence(args)
+    new_sentence = []
+  
+    i = 0
+    while i < args.length
+      word = args[i]
+      cleaned_word = clean_word(word)
+      new_sentence << cleaned_word
+      i += 1
+    end
+  
+    return new_sentence.join(" ")
+  end
+  
+  args = ARGV
+  
+  if args.length == 0
+    puts "error"
+    exit
+  else
+    result = clean_sentence(args)
+    puts result
+  end
+  
+
+  
+
+
