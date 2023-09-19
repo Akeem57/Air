91489d14fa8c9dddb93463f7418fde1c6a31b84a
Akeem57 <lyrlandais@hotmail.com>
2023-09-19 20:40:29 +0200

    Sur chacun d'entre eux

--- air05.rb
+++ air05.rb
@@ -12,3 +12,39 @@ L’opération à appliquer sera toujours le dernier élément.
 Aﬃcher error et quitter le programme en cas de problèmes d’arguments.
 =end
 
+def calculate_last_arg_on_a_list(arguments)
+    list = arguments[0...-1]
+    last_arg = arguments[-1]
+    operator = last_arg[0]
+    number = last_arg[1].to_i
+    i=0
+    resultat = []
+    while i < list.length
+        if operator == "+"
+            resultat[i] = list[i].to_i + number.to_i
+        elsif operator == "-"
+            resultat[i] = list[i].to_i - number.to_i
+        else
+            puts "error"
+            exit
+        end
+        i+=1
+    end
+    return resultat
+end
+
+if ARGV[-1].length != 2
+    puts "error"
+    exit
+elsif ARGV[-1][1].to_i.to_s != ARGV[-1][1]
+    puts "error"
+    exit
+elsif !ARGV[0...-1].all? { |arg| arg.to_i.to_s == arg }
+    puts "error"
+    exit
+end 
+
+
+arguments = ARGV
+resultat = calculate_last_arg_on_a_list(arguments)
+puts resultat.join(" ")
