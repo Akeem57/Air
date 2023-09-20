b3583b0afeeb0657a524b24563c7f879f3221540
Akeem57 <lyrlandais@hotmail.com>
2023-09-20 20:37:27 +0200

    Contrôle de pass sanitaire

--- air06.rb
+++ air06.rb
@@ -1,5 +1,6 @@
 =begin
-Contrôle de pass sanitaire
+CONTROLE DE PASS SANITAIRE
+
 Créez un programme qui supprime d’un tableau tous les éléments qui ne
 contiennent pas une autre chaîne de caractères.
 Utilisez une fonction de ce genre (selon votre langage) :
@@ -14,4 +15,32 @@ $> python exo.py “Michel” “Albert” “Thérèse” “Benoit” “a”
 Michel, Thérèse, Benoit
 Aﬃcher error et quitter le programme en cas de problèmes d’arguments.
 =end
+
+
+if ARGV.length < 1 || ARGV.any?{|arg| arg.match?(/\d+/)}
+    puts "error"
+    exit
+end
+
+def who_has_checkChar_goes_out(arguments,check_char)
+    i=0
+    result = []
+    while i < arguments.length
+        if !arguments[i].match?(/#{check_char}/i)
+            result << arguments[i] #on ajoute le mot correspondant au tableau
+        end
+        i+=1 
+    end
+    return result
+end
+
+arguments = ARGV[0...-1].map{|words| words.gsub(/["'“”]/, "")} #.map quand on agit sur un tableau
+check_char = ARGV[-1].gsub(/["'“”]/, "")
+
+
+final_result = who_has_checkChar_goes_out(arguments, check_char)
+puts final_result.join(", ")
+
+
+
 
