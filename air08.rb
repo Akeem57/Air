05ee91cfcff7dee6cc79c9579b2b008ec21d3617
Akeem57 <lyrlandais@hotmail.com>
2023-09-20 21:27:22 +0200

    début

---
+++ air08.rb
@@ -0,0 +1,15 @@
+=begin
+MELANGER 2 TABLEAUX TRIES
+
+Créez un programme qui fusionne deux listes d’entiers triées en les
+gardant triées, les deux listes seront séparées par un “fusion”.
+Utilisez une fonction de ce genre (selon votre langage) :
+sorted_fusion(array1, array2) {
+# your algo
+return (new_array)
+}
+Exemples d’utilisation :
+$> ruby exo.rb 10 20 30 fusion 15 25 35
+10 15 20 25 30 35
+Aﬃcher error et quitter le programme en cas de problèmes d’arguments.
+=end
