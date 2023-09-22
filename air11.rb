df4a90bb99bee967f6f84cf538e25b3a6d57d8a1
Akeem57 <lyrlandais@hotmail.com>
2023-09-22 10:19:27 +0200

    Afficher une pyramide

--- air11.rb
+++ air11.rb
@@ -12,3 +12,25 @@ OOOOOOO
 OOOOOOOOO
 Aﬃcher error et quitter le programme en cas de problèmes d’arguments.
 =end
+
+if ARGV.length < 2 || !ARGV[1].match?(/\A\d+\z/)
+    puts "error"
+    exit
+end
+
+def stairs_with_specific_char(char, long)
+    i=0
+    j=1
+    while i < long
+        puts (char)*j
+        i+=1
+        j+=2
+    end
+    return
+end
+
+char = ARGV[0]
+long = ARGV[1].to_i
+
+result = stairs_with_specific_char(char, long)
+print result
