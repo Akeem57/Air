0e1ceb5ea0c3650cf76f9ccee8cfb923ee5d84a8
Akeem57 <lyrlandais@hotmail.com>
2023-09-22 10:38:59 +0200

    Air terminé

---
+++ air13.rb
@@ -0,0 +1,12 @@
+=begin
+AIR : OK
+
+Créez un programme qui célèbre votre victoire.
+Exemples d’utilisation :
+$> python exo.py
+J’ai terminé l’Épreuve de l’Air et c’était [].
+$>
+Note : [] est à remplacer par un adjectif de votre choix (facile ?)
+=end
+
+puts "J'ai terminé l'Epreuve de l'air et j'étais plus à l'aise que l'eau"
